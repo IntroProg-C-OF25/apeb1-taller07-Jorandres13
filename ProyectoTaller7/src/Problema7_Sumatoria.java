0301aa2a1cf82c915ce6c527b4c82635de4650aa
Jorandres13 <jaguerrero39@utpl.edu.ec>
2024-11-22 09:01:36 -0500

    Ejercicios

--- ProyectoTaller7/src/Problema7_Sumatoria.java
+++ ProyectoTaller7/src/Problema7_Sumatoria.java
@@ -6,6 +6,7 @@ sumatoria = -(1/1)+(1/2)-(1/3)+(1/4)-(1/5)+(1/6)-(1/7)+(1/8)-(1/9)+(1/10)
 import java.util.Scanner;
 
 public class Problema7_Sumatoria {
+
     public static void main(String[] args) {
         Scanner tcl = new Scanner(System.in);
         int contador = 1, limite, contador2 = 0;
@@ -27,8 +28,4 @@ public class Problema7_Sumatoria {
 }
 /* Dame el limite: 4
 -(1 /1), +(1 /2), -(1 /3), +(1 /4), -(1 /5), 
-*/
-        
-
-    
-
+ */
